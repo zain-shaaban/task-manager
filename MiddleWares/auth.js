169b985e42e109e12875d3acaad56cda832a63a5
zein-shaaban <zeinshaban25@gmail.com>
2024-08-06 20:40:45 +0300

    fix author

--- MiddleWares/auth.js
+++ MiddleWares/auth.js
@@ -4,19 +4,15 @@ require("dotenv").config();
 const Autherizarion = function (req, res, next) {
   try {
     const token = req.body.token;
-    if (token) {
       const auth = jwt.verify(token, process.env.JWT_SECRET);
       if (auth) {
         req.UserId = auth.UserId;
         return next();
       }
-    }
-  } catch (error) {
-    return res.status(404).json({
-      status: 0,
-      message: "Not Autherized",
-    });
-  }
-};
+    }catch (error) {
+      error.message= "Not Autherized";
+      next(error);
+    };
+  };
 
 module.exports = { Autherizarion };
