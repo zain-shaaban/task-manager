bb7604030a04c7eb6d05964f698ec67953de4522
zein-shaaban <zeinshaban25@gmail.com>
2024-08-13 01:58:01 +0300

    fix problem

--- MiddleWares/auth.js
+++ MiddleWares/auth.js
@@ -9,10 +9,11 @@ const Autherizarion = asyncWrapper(async function (req, res, next) {
     const auth = jwt.verify(token, process.env.JWT_SECRET);
     if (auth) {
       const user=await User.findById(auth.UserId);
+      if(user){
       if(user.exp==-1||user.exp<=auth.exp){
         if(auth.update)
           await user.updateOne({exp:auth.exp})
-      }
+      }}
       else
         throw new costumeError("invalid token",400)
       req.UserId = auth.UserId;
