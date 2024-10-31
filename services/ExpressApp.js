0158a30575a292c9ed36f6ccbad5fe689ee60e09
zeinshaban <zeinshaban25@gmail.com>
2024-10-31 20:15:08 +0300

    fix helmet

--- services/ExpressApp.js
+++ services/ExpressApp.js
@@ -2,14 +2,15 @@ require("dotenv").config();
 const swaggerDOC = require("swagger-jsdoc");
 const swaggerUI = require("swagger-ui-express");
 const cors = require("cors");
+const helmet = require("helmet");
 const limiter = require("../MiddleWares/rateLimiter");
 const errorHandler = require("../MiddleWares/errorHandler");
 const notFound = require("../MiddleWares/404");
 const express = require("express");
 
-
 module.exports = async (app) => {
   app.use(cors());
+  app.use(helmet());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(limiter());
